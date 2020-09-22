f87f34bb62e297d947cf4bb848956b906ceb09fc
Vivekachooz <vivekachooz@gmail.com>
2020-09-22 12:01:14 +0530

    sdm660-common: Bringup EvolutionX
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>
    Change-Id: I6a3e5d42b8c3c517470a085535d6863953bea605

--- sdm660.mk
+++ sdm660.mk
@@ -285,6 +285,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evolution \
     $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-system
 
@@ -433,10 +434,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm660
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \

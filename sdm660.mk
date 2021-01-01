e0decf6ef3b4ded9e014d3a67dc7b69ac451efcd
Vivekachooz <vivekachooz@gmail.com>
2021-01-01 12:23:57 +0530

    Revert "sdm660-common: Switch back to previous thermal.sdm660"
    This reverts commit ce79f72cedb947b9e58071e862d94d413a7e54f6.
    
    Change-Id: I0353c9ecec5b7563ffa040134137d11e1092117b

--- sdm660.mk
+++ sdm660.mk
@@ -446,9 +446,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm660
+    android.hardware.thermal@2.0-service.qti
 
 # USB
 PRODUCT_PACKAGES += \
